b6d240840fa8fb8185a5627e071f7f63fdf73298
Qiongxuan <31785650+qxtan@users.noreply.github.com>
2018-12-03 20:30:25 -0500

    Add files via upload

---
+++ Code/main.py
@@ -0,0 +1,9 @@
+import tkinter as Tk
+import opencv as cv
+
+
+top = Tk.Tk()
+
+while True:
+	
+	top.update()
---
+++ Code/test_filter.py
@@ -0,0 +1,25 @@
+import cv2
+import time
+import filters
+
+cap = cv2.VideoCapture(0)
+
+print("Switch to video window. Then press 'p' to save image, 'q' to quit")
+
+while cap.isOpened():
+
+    [ok, frame] = cap.read()
+    frame = cv2.flip(frame, 1)
+    frame = filters.filterGrayScale(frame)
+
+    cv2.imshow('Live video', frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('p'):
+        cv2.imwrite('photo_{}.jpg'.format(time.strftime("%Y%m%d%H%M%S", time.localtime()) ), frame)
+    elif key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
