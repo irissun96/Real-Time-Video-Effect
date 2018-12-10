aab3d25f073a2cb1cc43a82f7a5a00f6beefe3b8
qxtan <qt310@nyu.edu>
2018-12-09 19:02:09 -0500

    add videoShadow and faceSticker w/ mouth using dlib

--- Code/test_filter.py
+++ Code/test_filter.py
@@ -6,6 +6,10 @@ from datetime import datetime
 
 cap = cv2.VideoCapture(0)
 
+[ok, frame] = cap.read()
+shadow = filters.videoShadow(buffer_size=16, delay_frames=3, alpha=0.5, frame_size=frame.shape)
+faceSticker = filters.faceSticker_dlib("../Image/sticker_file_0.txt")
+
 print("Switch to video window. Then press 'p' to save image, 'q' to quit")
 
 
@@ -20,15 +24,24 @@ while cap.isOpened():
     time_start = datetime.now()
     # frame = filters.filterGrayScale(frame)#Delay time: 0:00:00.001995
     
-    # frame = filters.filterRGBGlitchRandom(frame)#Delay time: 0:00:00.004999
+    # frame = filters.filterRGBGlitch_Random(frame)#Delay time: 0:00:00.004999
+
+    # frame = filters.skinDetection_HSV(frame)
 
     # frame = filters.skinSmooth_Bilateral_AlphaBlending(frame) # Delay time: 0:00:00.135920
 
     # frame = filters.skinSmooth_Gaussian_AlphaBlending(frame) # Delay time: 0:00:00.101941
 
     # frame = filters.faceEyeDetect(frame) # Delay time: 0:00:00.212875
+    # faces, faces_eyes = filters.faceEyeDetect(frame)
+    # frame = filters.faceEyeDraw(frame, faces, faces_eyes)
 
-    frame = filters.faceSticker(frame, "../Image/emoji_resized_0.png", 1.5) # Delay time: 0:00:00.167900
+    # frame = filters.faceSticker(frame, "../Image/Poop_Emoji_resized.png", 1.5) # Delay time: 0:00:00.167900
+
+    frame = faceSticker.newFrame(frame)
+
+    # frame = shadow.newFrame(frame)
+
 
     time_end = datetime.now()
     # ===================
